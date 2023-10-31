58b2c45d0175228be54135fc128ba96aa4301134
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-31 13:22:32 -0400

    type

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Giordan Zeina
+ * Created on: Oct 2023
+ * This program when you press the "A" button it counts down from 4 to 0 
+ * at each number, you have that many Neopixels light up
 */
 
-basic.showString('Hello, World!')
+// variables
+let loopCounter = 0
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+
+// press "A" button
+input.onButtonPressed(Button.A, function () {
+  // setup
+  basic.clearScreen()
+  loopCounter = 4
+  while (loopCounter <= 4) {
+    basic.pause(1000)
+    basic.showNumber(loopCounter)
+    loopCounter = loopCounter - 1
+  }
+  basic.showIcon(IconNames.Happy)
+})
