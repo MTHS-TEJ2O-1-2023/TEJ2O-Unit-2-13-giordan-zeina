aece72bcd1f2daea594158bd91635e0a11b817a3
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-11-14 13:20:29 -0500

    Typescript version

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
  * Created by: Giordan Zeina
  * Created on: Oct 2023
@@ -21,62 +21,23 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 // press "A" button
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function () {      
+  neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+  neopixelStrip.show()
+
   // setup
   loopCounter = 4
   while (loopCounter >= 0) {
     basic.pause(1000)
+    neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
     basic.showNumber(loopCounter)
-
-    // if loopCounter === 4 show 4 blue Neopixels
-    if (loopCounter === 4) {
-      neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.show()
-    }
-
-    // if loopCounter === 3 light up 3 blue Neopixels
-    if (loopCounter === 3) {
-      neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.show()
-    }
-
-    // if loopCounter === 2 light up 2 blue Neopixels
-    if (loopCounter === 2) {
-      neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.show()
-    }
-
-    // if loopCounter === 1 light up 1 blue Neopixels
-    if (loopCounter === 1) {
-      neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.show()
-    }
-
-    // if loopCounter === 0 light up 0 blue Neopixels
-    if (loopCounter === 0) {
-      neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-      neopixelStrip.show()
-    }
     loopCounter = loopCounter - 1
+    pause(500)
   }
+  basic.showIcon(IconNames.Happy)
 })
